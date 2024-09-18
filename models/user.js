4292ad9754eab3af9c0e2edd14aef42e13a3aad8
saadkhan011 <saadaslam6866@gmail.com>
2024-09-18 23:02:04 +0500

    release-date-added

--- models/user.js
+++ models/user.js
@@ -22,6 +22,9 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: false,
     },
+    releaseDate:{
+      type: Date,
+    },
     password: {
       type: String,
       required: false,
